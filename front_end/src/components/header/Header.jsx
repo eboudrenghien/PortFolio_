c7eebc8c7e8c7830b7361971293a01ec9115b356
Esther Boudrenghien <e.boudrenghien@codeur.online>
2022-05-13 16:52:08 +0200

    mise en forme des catégories + histoire du site

--- front_end/src/components/header/Header.jsx
+++ front_end/src/components/header/Header.jsx
@@ -14,13 +14,7 @@ function Header() {
         <div className="actualites">Photos des actualités</div>
         <div className="implants">Photo implant</div>
       </div>
-      <div className="histoire">
-        <h1>HISTOIRE</h1>
-        <p>La surdité selon Esther, pourquoi ce titre ? Au départ, je ne pensais pas créer un site sur la surdité. 
-        Je me suis rendue compte que nous avions pas beaucoup de retour sur la surdité, l'implantation, les témoignages de l'implanté mais aussi des proches.
-        C'est pourquoi, je me suis dit que j'allais parlé de ma surdité, du passé, du présent, du futur avec ma surdité. 
-        Mais pas seulement, avec l'accord, les personnes entendants, malentendants, sourds pourront également témoigner de leurs parcours, de leurs craintes. à compléter </p>
-      </div>
+
       
     </div>
   )
