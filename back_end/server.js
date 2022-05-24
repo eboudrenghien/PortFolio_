af99bc50efd66484075bd20afe1ef4959b737ac4
Esther Boudrenghien <e.boudrenghien@codeur.online>
2022-05-24 13:50:00 +0200

    ajout cours.odt + ajout création d'article + suppression d'une collection

--- back_end/server.js
+++ back_end/server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.URL, {useNewUrlParser: true})
+mongoose.connect(process.env.URL_API, {useNewUrlParser: true})
 
 const articleSchema = {
     titre : String,
@@ -20,13 +20,38 @@ const articleSchema = {
 }
 const Article = mongoose.model("Article", articleSchema)
 //TODO
+// affiche la collection "articles"
 
-app.get("/articles", (req, res) => {
+app.get("/actualites", (req, res) => {
     Article.find((err, foundArticles) => {
-        console.log(foundArticles);
+        if(!err) {
+            res.send(foundArticles)
+        } else {
+            res.send(err)
+        }
     })
 })
 
-app.listen(5000, function() {
-  console.log("Le serveur est connecté sur le port 5000 => http://localhost:5000/articles");
+// créer un article dans la collection "articles"
+app.post("/actualites", (req, res) => {
+
+   const newArticle = new Article ({
+        titre: req.body.titre,
+        contenu: req.body.contenu
+   })
+   newArticle.save((err) => {
+       if (!err) {
+           res.send("Votre nouvel article a été ajouté avec succès")
+       } else {
+           res.send(err)
+       }
+   })
+})
+
+// supprime la collection "articles"
+app.delete("/actualites", (req, res) => {
+    
+})
+app.listen(process.env.PORT, function() {
+  console.log("Le serveur est connecté sur le port 5000 => http://localhost:5000/actualites");
 });
